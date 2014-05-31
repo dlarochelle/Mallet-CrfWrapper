5136d30e35550b60fdd5ce833300d672261613e9
David <dlarochelle@cyber.law.harvard.edu>
2014-05-31 15:17:16 -0400

    Fix required perl version.

--- lib/Mallet/CrfWrapper.pm
+++ lib/Mallet/CrfWrapper.pm
@@ -1,12 +1,12 @@
 package Mallet::CrfWrapper;
 
-use 5.006;
+use 5.14.0;
 use strict;
-
+use warnings;
 
 =head1 NAME
 
-Mallet::CrfWrapper - The great new Mallet::CrfWrapper!
+Mallet::CrfWrapper 
 
 =head1 VERSION
 
@@ -19,14 +19,9 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This module provides a convenient wrapper around Mallet's CRF package.
 
-Perhaps a little code snippet.
-
-    use Mallet::CrfWrapper;
-
-    my $foo = Mallet::CrfWrapper->new();
-    ...
+It can use either Inline::Java or a WebService to interface with Mallet.
 
 =head1 EXPORT
 
@@ -37,12 +32,12 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head2 function1
 
+=cut
+
 #
 # Facade to either Inline::Java based or a web service-based CRF model runner
 #
 
-use strict;
-use warnings;
 
 # Name of a loaded and active CRF module, either 'Mallet::CrfWrapper::InlineJava' or
 # 'Mallet::CrfWrapper::WebService'.
@@ -200,47 +195,13 @@ L<http://search.cpan.org/dist/Mallet-CrfWrapper/>
 
 =head1 ACKNOWLEDGEMENTS
 
+Linas Valiukas made numerous performance improvements and added the WebService code.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 David Larochelle.
+Copyright 2014 Berkman Center for Internet & Society at Harvard University.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+This distribution includes JAR files for Mallet which are licensed under the Common Public License and are Copyright UMass Umherst.
 
 =cut
 
