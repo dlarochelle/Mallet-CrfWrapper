40397daaea12ae1e68b6b572663cb6d3fee269b4
Linas Valiukas <shirshegsm@gmail.com>
2014-06-03 00:25:16 +0300

    Changelog, version 0.02

--- lib/Mallet/CrfWrapper.pm
+++ lib/Mallet/CrfWrapper.pm
@@ -10,11 +10,11 @@ Mallet::CrfWrapper - a convenient wrapper around Mallet's CRF package.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
